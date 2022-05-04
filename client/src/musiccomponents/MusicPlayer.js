8382b36674dd1742af38523c07c560aafe3a16a0
Matt <mckaymatt@outlook.com>
2022-05-04 16:07:01 -0400

    Day 5 Music Player Working

--- client/src/musiccomponents/MusicPlayer.js
+++ client/src/musiccomponents/MusicPlayer.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SpotifyPlayer from "react-spotify-web-playback"
 
 function MusicPlayer({accessToken, trackUri}) {
+    const [play, setPlay] = useState(false)
 
-    console.log("access token in player", accessToken)
+    useEffect(() => setPlay(true), [trackUri])
+
+    // console.log("access token in player", accessToken)
 
     if (!accessToken) return null
     return ( 
         <SpotifyPlayer 
             token={accessToken}
             showSaveIcon
+            callback={state => {
+                if (!state.isPlaying) setPlay(false)
+            }}
+            play={play}
             uris={trackUri ? [trackUri] : []}
         /> 
     );
