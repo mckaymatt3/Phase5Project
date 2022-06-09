9a84f29978e9c491b1795ac2bf46c7f44f6db86b
mckaymatt3 <mckaymatt@outlook.com>
2022-06-09 16:00:50 -0400

    Updated cleanup

--- client/src/user/Login.js
+++ client/src/user/Login.js
@@ -15,7 +15,7 @@ function Login({username, setUsername, password, setPassword, login, setLogin, u
 
     const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.user.value);
-    console.log("user", user)
+    // console.log("user", user)
     
     function handleSubmit(e){
         e.preventDefault()
@@ -32,7 +32,7 @@ function Login({username, setUsername, password, setPassword, login, setLogin, u
         })
         .then(response => response.json())
         .then(jsonData => {
-            console.log("Data here:", jsonData);
+            // console.log("Data here:", jsonData);
             setUser(user);
             dispatch(setValue(user))
             setIsLoading(false)  
